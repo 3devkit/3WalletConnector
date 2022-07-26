676d50dd29fc2f28e6f8666137199869fac52849
3walle <web3.walle@gmail.com>
2022-07-27 01:06:56 +0800

    fix

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,10 +1,8 @@
 export * from './provider';
 export * from './context';
 
-// export * from '@3walletconnector/core';
-// export * from '@3walletconnector/helpers';
-// export * from '@3walletconnector/react-ui';
-// export * from '@3walletconnector/wallet-metamask';
-// export * from '@3walletconnector/wallet-phantom';
-
-// test2
+export * from '@3walletconnector/core';
+export * from '@3walletconnector/helpers';
+export * from '@3walletconnector/react-ui';
+export * from '@3walletconnector/wallet-metamask';
+export * from '@3walletconnector/wallet-phantom';
